a2dc4513385b5dd5aba320a9eba8218a32dd8563
Lukasz Nowakowski <lukrzak@wp.pl>
2024-09-09 21:17:14 +0200

    Added semi-random uuid generator with constant prefix

--- stos_mock/stos.py
+++ stos_mock/stos.py
@@ -1,11 +1,22 @@
+import random
 from random import randint
 from os import environ
-from uuid import uuid4
 from flask import Flask, jsonify, send_file
 from pathlib import Path
 
 
 app = Flask(__name__)
+
+
+def get_semi_random_uuid(last_char: str = None):
+    '''
+    Rerurns valid UUID with randomized last character, if parameter is not specified
+    :return: Returns UUID as string value
+    '''
+    ID_PREFIX = "11111111-2222-3333-4444-55555555555"
+    last_character = last_char if last_char is not None else str(randint(0, 9))
+
+    return ID_PREFIX + last_character
 
 
 @app.route("/tasks", methods=['GET'])
@@ -15,9 +26,9 @@ def tasks():
     :return: Task's data in JSON format.
     """
     randomized_task: dict = {
-        "student_id": str(randint(1, 100)),
-        "task_id": str(uuid4()),
-        "file_ids": [str(uuid4()) for _ in range(randint(1, 5))]
+        "student_id": str(randint(1, 10)),
+        "task_id": get_semi_random_uuid(random.choice(['A', 'B', 'C', 'D', 'E', 'F'])),
+        "file_ids": [get_semi_random_uuid() for _ in range(randint(1, 5))]
     }
 
     return jsonify(randomized_task), 200
@@ -45,6 +56,7 @@ def task(task_id: str):
     :return: Result message in JSON format.
     """
     return jsonify({"result": "uploaded " + str(task_id)}), 200
+
 
 PORT = int(environ.get("STOS_PORT", '2137'))
 HOST = environ.get("STOS_HOST", '127.0.0.1')
