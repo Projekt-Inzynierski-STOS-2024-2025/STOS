82d7483625992cc41a97cb2ac5b84df369752ffe
tpalinski <tpalinski@icloud.com>
2024-10-04 13:53:09 +0200

    [STS-5] Add system healthcheck to deployment

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -3,14 +3,21 @@ current_dir=$(dirname "$(realpath "$0")")
 cd $current_dir
 root_dir=$(dirname "$current_dir")
 ns=$(which nix-shell)
-echo --- Running testing.sh ---
+echo --- Running healthcheck.sh 
+/bin/bash healthcheck.sh
+if [ $? -ne 0 ]; then
+    echo --- ERROR - healthcheck failed, exiting
+    exit 1
+fi
+echo --- System healthy 
+echo --- Running testing.sh 
 nix-shell shell.testing.nix --run "./test.sh"
 if [ $? -ne 0 ]; then
     echo --- ERROR - testing.sh exited with a non-zero exit code, aborting
     exit 1
 fi
-echo --- Entering root environment ---
-echo --- Generating stos startup script for systemd ---
+echo --- Entering root environment 
+echo --- Generating stos startup script for systemd 
 sudo touch /usr/bin/stos-bootstrap
 sudo tee /usr/bin/stos-bootstrap > /dev/null << EOF
 #!/bin/bash
@@ -20,11 +27,11 @@ $ns $current_dir/shell.nix --run 'python3 main.py' >> /home/stos/stos.log &
 wait
 EOF
 sudo chmod +x /usr/bin/stos-bootstrap
-echo --- Updating service configuration ---
+echo --- Updating service configuration 
 sudo cp ./stos.service /etc/systemd/system/stos.service
-echo --- Starting stos.service ---
+echo --- Starting stos.service 
 sudo systemctl daemon-reload
 sudo systemctl restart stos
 sleep 1
-echo --- Done, showing service status ---
+echo --- Done, showing service status 
 sudo systemctl status stos
