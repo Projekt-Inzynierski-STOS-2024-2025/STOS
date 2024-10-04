20c74d2dc351f09e839e2bbe41b9837e35451844
tpalinski <tpalinski@icloud.com>
2024-10-04 13:53:09 +0200

    [STS-5] Add system healthcheck to deployment

--- deployment/test.sh
+++ deployment/test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env nix-shell
 #!nix-shell shell.testing.nix -i bash
-echo ------ Running pytest for the entire project ------
+echo ------ Running pytest for the entire project 
 parent_dir=$(dirname "$(realpath "$0")")/..
 cd "$parent_dir"
 pytest
 if [ $? -ne 0 ]; then
-    echo ------ ERROR - tests failed, aborting ------
+    echo ------ ERROR - tests failed, aborting 
     exit 1
 else
-    echo ------ Finished running tests successfully ------
+    echo ------ Finished running tests successfully 
 fi
